936c5bc486cfbf8b6cbee31ab9c28c15e5b3f08e
Taylor Mott <trm36@humboldt.edu>
2015-05-15 22:15:06 -0600

    Initial Commit

---
+++ TimeTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeTracker
+//
+//  Created by Taylor Mott on 15 May 15.
+//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
