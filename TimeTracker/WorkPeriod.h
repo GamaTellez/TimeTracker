034cba20ab546714a1bfffbf1c8c0f7d7e776539
Taylor Mott <trm36@humboldt.edu>
2015-05-15 22:26:41 -0600

    Adds WorkPeriod model object
    Work Period.h
    Properties:
    - NSDate *startDate
    - NSDate *endDate
    - NSString *memo
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ TimeTracker/WorkPeriod.h
@@ -0,0 +1,13 @@
+//
+//  WorkPeriod.h
+//  TimeTracker
+//
+//  Created by Taylor Mott on 15 May 15.
+//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WorkPeriod : NSObject
+
+@end
---
+++ TimeTracker/WorkPeriod.m
@@ -0,0 +1,13 @@
+//
+//  WorkPeriod.m
+//  TimeTracker
+//
+//  Created by Taylor Mott on 15 May 15.
+//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//
+
+#import "WorkPeriod.h"
+
+@implementation WorkPeriod
+
+@end
