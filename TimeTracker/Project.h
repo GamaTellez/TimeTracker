009690c183930be0356877c9089585cb6c8902c6
Taylor Mott <trm36@humboldt.edu>
2015-05-19 15:01:52 -0600

    Makes workPeriods Relationship be ordered
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- TimeTracker/Project.h
+++ TimeTracker/Project.h
@@ -9,18 +9,24 @@
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class WorkPeriod;
 
 @interface Project : NSManagedObject
 
 @property (nonatomic, retain) NSString * title;
-@property (nonatomic, retain) NSSet *workPeriods;
+@property (nonatomic, retain) NSOrderedSet *workPeriods;
 @end
 
 @interface Project (CoreDataGeneratedAccessors)
 
-- (void)addWorkPeriodsObject:(NSManagedObject *)value;
-- (void)removeWorkPeriodsObject:(NSManagedObject *)value;
-- (void)addWorkPeriods:(NSSet *)values;
-- (void)removeWorkPeriods:(NSSet *)values;
-
+- (void)insertObject:(WorkPeriod *)value inWorkPeriodsAtIndex:(NSUInteger)idx;
+- (void)removeObjectFromWorkPeriodsAtIndex:(NSUInteger)idx;
+- (void)insertWorkPeriods:(NSArray *)value atIndexes:(NSIndexSet *)indexes;
+- (void)removeWorkPeriodsAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInWorkPeriodsAtIndex:(NSUInteger)idx withObject:(WorkPeriod *)value;
+- (void)replaceWorkPeriodsAtIndexes:(NSIndexSet *)indexes withWorkPeriods:(NSArray *)values;
+- (void)addWorkPeriodsObject:(WorkPeriod *)value;
+- (void)removeWorkPeriodsObject:(WorkPeriod *)value;
+- (void)addWorkPeriods:(NSOrderedSet *)values;
+- (void)removeWorkPeriods:(NSOrderedSet *)values;
 @end
--- TimeTracker/Project.m
+++ TimeTracker/Project.m
@@ -7,6 +7,7 @@
 //
 
 #import "Project.h"
+#import "WorkPeriod.h"
 
 
 @implementation Project
