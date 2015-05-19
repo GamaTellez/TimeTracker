f9ab2a0e05efb88243f657ec61f4e0e175c2bc68
Taylor Mott <trm36@humboldt.edu>
2015-05-19 14:33:16 -0600

    Adds Stack class
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ TimeTracker/Stack.h
@@ -0,0 +1,13 @@
+//
+//  Stack.h
+//  TimeTracker
+//
+//  Created by Taylor Mott on 19 May 15.
+//  Copyright (c) 2015 Mott Applications. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
+@end
